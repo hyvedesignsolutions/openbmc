6720730a61020ea334cedd1d84a748d7b322ed20
Brad Chou <bradc@hyvedesignsolutions.com>
2019-09-26 16:36:04 +0800

    meta-hs2500: Add lamp_test button. Lamp_test button is defined as SW704. It triggers the lamp_test led group. Press SW704 to toggle lamp_test and blink all LEDs.

---
+++ meta-hdsc/meta-hs2500/recipes-phosphor/gpio/lamp-test/toggle_lamp_test.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Toggle the state of lamp_test LED Group
+
+SERVICE="xyz.openbmc_project.LED.GroupManager"
+INTERFACE="xyz.openbmc_project.Led.Group"
+PROPERTY="Asserted"
+
+# Get current state
+object=$(busctl tree $SERVICE --list | grep lamp_test)
+state=$(busctl get-property $SERVICE $object $INTERFACE $PROPERTY \
+	  |  awk '{print $NF;}')
+
+if [ "$state" == "false" ]; then
+    target='true'
+else
+    target='false'
+fi
+
+# Set target state
+busctl set-property $SERVICE $object $INTERFACE $PROPERTY b $target
