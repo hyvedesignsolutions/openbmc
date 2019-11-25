6f8f483d7467b00d378d50f886c7aa9f91face18
Jeannie Wu <jeanniew@hyvedesignsolutions.com>
2019-11-25 10:43:50 +0800

    [HS9216] Add PowerStatus Check function in Caterr trigger function
    TESTED:
     Execute `ipmitool power off` and check sel log (CATTER not trigger in power off status)

--- meta-hdsc/meta-hs9216/recipes-phosphor/gpio/caterr-update/caterr_update.cpp
+++ meta-hdsc/meta-hs9216/recipes-phosphor/gpio/caterr-update/caterr_update.cpp
@@ -11,13 +11,47 @@
 #define CATERR_INTERFACE "xyz.openbmc_project.Control.Processor"
 #define PROPERTY_INTERFACE "org.freedesktop.DBus.Properties"
 
+#define CHASSIS_STATE_OBJECT "/xyz/openbmc_project/state/chassis0"
+#define CHASSIS_SERVICE "xyz.openbmc_project.State.Chassis"
+#define CHASSIS_INTERFACE "xyz.openbmc_project.State.Chassis"
+
+
+static int getPropertyString(sdbusplus::bus::bus& bus, const std::string& path,
+       const std::string& property, std::string& value, const std::string service, const std::string interface)
+{
+    auto method = bus.new_method_call(service.c_str(), path.c_str(), PROPERTY_INTERFACE, "Get");
+    method.append(interface.c_str(),property);
+    auto reply=bus.call(method);
+    if (reply.is_method_error())
+    {
+        std::printf("Error looking up services, PATH=%s",interface.c_str());
+        return -1;
+    }
+
+    sdbusplus::message::variant<std::string> valuetmp;
+    try
+    {
+        reply.read(valuetmp);
+    }
+    catch (const sdbusplus::exception::SdBusError& e)
+    {
+        std::printf("Failed to get pattern string for match process");
+        return -1;
+    }
+
+    value = std::get<std::string>(valuetmp);
+    return 0;
+}
+
 /**
  * @brief Main
  */
 int main(int argc, char *argv[])
 {
     char direction[20]={0};
-    int assertflag=0;
+    char powerpath[100]={0};
+    std::string responseData;
+    int assertflag=0, ret=0;
 
     std::string state="xyz.openbmc_project.Control.Processor.State.NORMAL";
 
@@ -33,6 +67,17 @@ int main(int argc, char *argv[])
     }
 
     auto bus = sdbusplus::bus::new_default();
+    
+    //Get power status first, if power is off , ignore the function and exit.
+    ret = getPropertyString(bus,CHASSIS_STATE_OBJECT,"CurrentPowerState",responseData,CHASSIS_SERVICE,CHASSIS_INTERFACE);
+    auto index = responseData.find_last_of(".");
+    std::string psstate = responseData.substr(index+1);
+    std::printf("Jeannie test power state=%s\n",psstate.c_str());
+    if(psstate == "Off")
+    {
+        return 0;
+    }
+
     auto method = bus.new_method_call(CATERR_SERVICE, CATERR_OBJECTPATH_BASE,
                                       PROPERTY_INTERFACE, "Set");
     method.append(CATERR_INTERFACE,"ProcessorStatus", sdbusplus::message::variant<std::string>(state));
