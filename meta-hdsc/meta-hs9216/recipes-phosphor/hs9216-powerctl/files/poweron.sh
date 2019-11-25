ea0c449b941dc628145f2b385c3c872820f6b3b3
Jeannie Wu <jeanniew@hyvedesignsolutions.com>
2019-11-25 18:21:47 +0800

    [HS9216] Fix Passthrough setting for power/reset button   Original setting force turn off passthrough mode and lead to poweron/off problem with button.
    TESTED:
      1. In power on status, press power button and make sure system power off normal.
      2. In power off status, press power button and make sure system power on normal.
      3. Execute ipmitool power off/on command and make sure it react normal.

--- meta-hdsc/meta-hs9216/recipes-phosphor/hs9216-powerctl/files/poweron.sh
+++ meta-hdsc/meta-hs9216/recipes-phosphor/hs9216-powerctl/files/poweron.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
 
+power_on_mask=0x2000
+
+Control_PT_funtion(){
+/sbin/devmem 0x1e6e2000 32 0x1688A8A8
+reg=$(/sbin/devmem 0x1e6e208c)
+
+if [ $1 == "disable" ]; then
+   echo "Disable GPIO E2/E3 pass-through function (Bit 13)"
+   result=$((${reg} & $((~${power_on_mask}))))
+elif [ $1 == "enable" ]; then
+   echo "Enable GPIO E2/E3 pass-through function (Bit 13)"
+   result=$((${reg} | ${power_on_mask}))
+fi
+
+result_base16=$(printf "0x%x" $result)
+/sbin/devmem 0x1e6e208c 32 $result_base16
+#/sbin/devmem 0x1e6e208c 32 0x00001000
+/sbin/devmem 0x1e6e2000 32 0x00000001
+echo "Display reg SCU80 value:" $(/sbin/devmem 0x1e6e208c);
+}
+
+
 echo "Enter Power on System action"
 
 #USE GPIOS1 to check power status
 pwrstatus=$(/usr/bin/gpioget gpiochip0 145)
 if [ $pwrstatus -eq 0 ]; then
+
+    Control_PT_funtion disable
     # *** Push power button ***
     # GPIO E3 for power on
     /usr/bin/gpioset gpiochip0 35=0
@@ -26,6 +50,9 @@ if [ $pwrstatus -eq 0 ]; then
         fi
         (( CHECK=CHECK+1 ))
     done
+
+    Control_PT_funtion enable
+
 fi
 
 echo "Exit Power on System action"
--- meta-hdsc/meta-hs9216/recipes-phosphor/hs9216-powerctl/files/powerreset.sh
+++ meta-hdsc/meta-hs9216/recipes-phosphor/hs9216-powerctl/files/powerreset.sh
@@ -1,5 +1,27 @@
 /bin/bash
 
+
+E0_mask=0x1000
+
+Control_PT_funtion(){
+/sbin/devmem 0x1e6e2000 32 0x1688A8A8
+reg=$(/sbin/devmem 0x1e6e208c)
+
+if [ $1 == "disable" ]; then
+   echo "Disable GPIO E0/E1 pass-through function (Bit 12)"
+   result=$((${reg} & $((~${E0_mask}))))
+elif [ $1 == "enable" ]; then
+   echo "Enable GPIO E0/E1 pass-through function (Bit 12)"
+   result=$((${reg} | ${E0_mask}))
+fi
+
+result_base16=$(printf "0x%x" $result)
+/sbin/devmem 0x1e6e208c 32 $result_base16
+#/sbin/devmem 0x1e6e208c 32 0x00001000
+/sbin/devmem 0x1e6e2000 32 0x00000001
+echo "Display reg SCU80 value:" $(/sbin/devmem 0x1e6e208c);
+}
+
 echo "Enter Power Reset System Action"
 
 SERVICE="xyz.openbmc_project.Logging.IPMI"
@@ -10,6 +32,8 @@ METHOD="IpmiSelAdd"
 pwrstatus=$(/usr/bin/gpioget gpiochip0 145)
 if [ $pwrstatus -eq 1 ]; then
 
+    Control_PT_funtion disable
+
     # *** Reset ***
     /usr/bin/gpioset gpiochip0 33=0
     sleep 1
@@ -17,6 +41,7 @@ if [ $pwrstatus -eq 1 ]; then
 
     busctl call $SERVICE $OBJECT $INTERFACE $METHOD ssaybq "SEL Entry" "/xyz/openbmc_project/sensors/systemboot/SYSTEM_RESTART" 3 {0x07,0xff,0xff} yes 0x20
 
+    Control_PT_funtion enalbe
 else
     echo "System is off state."
 
